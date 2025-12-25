1820f025be3f2ca614795eb2a79ecf5330496cc0
marie <168664981+mariecjl@users.noreply.github.com>
2025-12-25 15:56:35 -0500

    Update cnnmodel.py

--- cnn/cnnmodel.py
+++ cnn/cnnmodel.py
@@ -5,30 +5,34 @@ class EmotionCNN(nn.Module):
     def __init__(self, num_classes=7):
         super().__init__()
 
+        #convolutional layer -- 3 blocks of conv2D, batchnorm, relu and pooling
         self.conv = nn.Sequential(
+            #first block
             nn.Conv2d(1, 32, 3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
+            #second block
             nn.Conv2d(32, 64, 3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),
-
+            #third block
             nn.Conv2d(64, 128, 3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2),
         )
 
+        #dense layers of linear, relu and dropout
         self.fc = nn.Sequential(
             nn.Linear(128 * 6 * 6, 256),
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(256, num_classes)
         )
-
+    #forward prop
     def forward(self, x):
         x = self.conv(x)
         x = x.view(x.size(0), -1)
