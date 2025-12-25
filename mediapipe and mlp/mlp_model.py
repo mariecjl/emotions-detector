e32cf0d1e7b9edf92957b2e3a742f0a8d95e91b2
marie <168664981+mariecjl@users.noreply.github.com>
2025-12-25 10:58:37 -0500

    Update mlp_model.py

--- mediapipe and mlp/mlp_model.py
+++ mediapipe and mlp/mlp_model.py
@@ -4,9 +4,12 @@ import torch.nn as nn
 class EmotionNet(nn.Module):
     def __init__(self, num_classes=7):
         super().__init__()
+        #mlp design
         self.net = nn.Sequential(
+            #linear, fully connected layer
             nn.Linear(1434, 256),
             nn.ReLU(),
+            #dropouts
             nn.Dropout(0.3),
             nn.Linear(256, 128),
             nn.ReLU(),
@@ -14,5 +17,6 @@ class EmotionNet(nn.Module):
             nn.Linear(128, num_classes)
         )
 
+    #forward prop
     def forward(self, x):
         return self.net(x)
